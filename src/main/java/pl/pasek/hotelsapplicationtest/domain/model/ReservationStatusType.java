fdb8f677f61f4a3900e192e64deaa9183845c8f6
matt-pasek <mpasek@tm1.edu.pl>
2022-02-22 09:23:39 +0100

    Initial commit
    Signed-off-by: matt-pasek <mpasek@tm1.edu.pl>

---
+++ src/main/java/pl/pasek/hotelsapplicationtest/domain/model/ReservationStatusType.java
@@ -0,0 +1,5 @@
+package pl.pasek.hotelsapplicationtest.domain.model;
+
+public enum ReservationStatusType {
+    NEW, READY, CHECKED_IN, CHECKED_OUT
+}
---
+++ src/main/java/pl/pasek/hotelsapplicationtest/remote/rest/dto/response/ReservationCollectionDto.java
@@ -0,0 +1,22 @@
+package pl.pasek.hotelsapplicationtest.remote.rest.dto.response;
+
+import java.util.List;
+
+public class ReservationCollectionDto {
+    private List<ReservationDto> reservations;
+
+    public ReservationCollectionDto(){
+    }
+
+    public ReservationCollectionDto(List<ReservationDto> reservations){
+        this.reservations = reservations;
+    }
+
+    public List<ReservationDto> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<ReservationDto> reservations) {
+        this.reservations = reservations;
+    }
+}
